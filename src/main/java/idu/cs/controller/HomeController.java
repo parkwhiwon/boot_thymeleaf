c4c0b8b8f15f5cdc1b10f894266f87455c1312bd
jjy <jjy0819jj@gmail.com>
2019-05-22 17:44:13 +0900

    commit 2019 05 22

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -1,11 +1,16 @@
 package idu.cs.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -19,12 +24,10 @@ public class HomeController {
 	
 	@GetMapping("/")
 	public String home(Model model) {
-		model.addAttribute("test", "인덕 컴소");
-		model.addAttribute("egy", "유응구");
 		return "index";
 	}
 	
-	@GetMapping("/register")
+	@GetMapping("/user-reg-form")
 	public String getRegForm(Model model) {
 		return "form";
 	}
@@ -47,10 +50,35 @@ public class HomeController {
 	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
 			throws ResourceNotFoundException {
 		User user = userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
-		model.addAttribute("id", user.getId());
-		model.addAttribute("name", user.getName());
-		model.addAttribute("company", user.getCompany());
+		model.addAttribute("user", user);
+		
 		return "user";
 		//return ResponseEntity.ok().body(user);
+	}
+	
+	@GetMapping("/users/fn")
+	public String getUserByName(@Param(value = "name") String name, Model model)
+			throws ResourceNotFoundException {
+		List<User> users = userRepo.findByName(name);
+		model.addAttribute("users", users);
+		return "userlist";
+	}
+	
+	@PutMapping("/users/{id}")
+	public String updateUser(@PathVariable(value = "id") Long userId, @Valid User userDetails, Model model) {
+		User user = userRepo.findById(userId).get(); // user는 DB로 부터 읽어온 객체
+		user.setName(userDetails.getName()); // userDetails는 전송한 객체
+		user.setCompany(userDetails.getCompany());
+		userRepo.save(user);
+		model.addAttribute("users", userRepo.findAll());
+		return "redirect:/users";
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public String deleteUser(@PathVariable(value = "id") Long userId, Model model) {
+		User user = userRepo.findById(userId).get();
+		userRepo.delete(user);
+		model.addAttribute("name", user.getName());
+		return "user-deleted";
 	}
 }
